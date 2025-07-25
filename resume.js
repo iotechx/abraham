4e70ef92388f5eb007ef6f560f2714fc19ef6253
Medium Technica <55025669+medium-technica@users.noreply.github.com>
2025-07-25 07:25:34 +0530

    update

--- resume.js
+++ resume.js
@@ -451,7 +451,7 @@ function fnStr2LinkHTML(str) {
 	return str;
 }
 
-export function fnReplaceKeywords2Links(textWithKeywords) {
+function fnReplaceKeywords2Links(textWithKeywords) {
 	const strS = "<l>";
 	const strE = "</l>";
 	let strLinkKeyword = textWithKeywords;
